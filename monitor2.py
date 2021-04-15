2cdc50fc326cf1c401b74b5c211fda29985d1036
Sapti Sunil <sapti.s@innoneur.com>
2021-04-15 19:27:37 +0530

    time frame for aeris is updated

--- monitor2.py
+++ monitor2.py
@@ -23,7 +23,6 @@ import json
 inputdir = os.environ.get("INPUTDIR")
 outputdir = os.environ.get("OUTPUTDIR")
 s = sched.scheduler(time.time, time.sleep)
-#print(outputdir)
 
 def function_cds(start_year,end_year,start_month,end_month,start_day,end_day,North,South,East,West,variables,output_format,time):
     c = cdsapi.Client()
@@ -123,10 +122,11 @@ def check_new(sc):
             #print("Error False")
             error = False
 
-
+        #Ouput Directory is made with the request id
         if not(os.path.exists(str(outputdir) + '/' + str(request_lkup['_id']))):
             os.makedirs(str(outputdir) + '/' + str(request_lkup['_id']))
-
+            
+        #Parameters are extracted from the request
         start_day = request_lkup['startdate'][6:8]
         end_day = request_lkup['enddate'][6:8]
         start_month = request_lkup['startdate'][4:6]
@@ -141,6 +141,7 @@ def check_new(sc):
         output = request_lkup['outputformat']
         email = request_lkup['email']
 
+        #Sending the first mail with the parameters
         message = Mail(from_email='devteam-noreply@hashdev.in',to_emails=email,subject='Microclim Request is submitted Succesfully',html_content='''Your Request was submitted with paramaters <br><br>
         Start date : ''' + str(start_day)+" "+str(start_month)+" "+str(start_year)+'''<br> End date : ''' + str(end_day)+" "+ str(end_month)+" " +str(end_year) +
         '''<br> Bounding Box :<br>   Latitude : ''' + str(North)+", "+str(South)+'''<br>   Longitude : '''+str(East)+", "+str(West)+'''<br> Interval : '''+str(interval)+ '''<br> Output format : '''+ str(output)  )
@@ -152,18 +153,26 @@ def check_new(sc):
             print(response.headers)   
         except Exception as e:
             print(e)
-            
+
+        #If the source type is aeris weather
         if request_lkup['sourcetype'] == 'aeris':
-            #print('Aeris')
+            #variables needed for the aeries request is extracted 
             North = request_lkup['lats'][0]
             East = request_lkup['lats'][1]
             South = request_lkup['longs'][0]
             West = request_lkup['longs'][1]
             email = request_lkup['email']
-            start_day = request_lkup['enddate'][6:8]
+            start_day = request_lkup['startdate'][6:8]
             start_month = request_lkup['startdate'][4:6]
             start_year = request_lkup['startdate'][:4]
+            end_day = request_lkup['enddate'][6:8]
+            end_month = request_lkup['enddate'][4:6]
+            end_year = request_lkup['enddate'][:4]
             variable = request_lkup['variable']
+            
+            location = str(North)+','+str(South)+','+str(East)+','+str(West)
+            start_date = str(start_month)+'/'+str(start_day)+'/'+str(start_year)
+            end_date = str(end_month)+'/'+str(end_day)+'/'+str(end_year)
             
             v1=' '
             v2=' '
@@ -180,24 +189,25 @@ def check_new(sc):
                     v3='periods.ob.windDir,'
                 if i=='Solar Radiation':
                     v5='periods.ob.solradWM2'
-            #print(v1,v2,v3)
             v4 =v1+v2+v3+v5
-            #print(v4)
-            request = urllib.request.urlopen('https://api.aerisapi.com/observations/archive/within?p='+str(North)+','+str(South)+','+str(East)+','+str(West)+'&from='+str(start_month)+'/'+str(start_day)+'/'+str(start_year)+'&format=json&filter=allstations&limit=10&plimit=1&fields='+v4+'&client_id= client_id &client_secret= client_secret')
+
+            #Aeris weather
+            request = urllib.request.urlopen('https://api.aerisapi.com/observations/archive/within?p='+location+'&from='+start_date+'&to='+end_date+'&format=json&filter=allstations&limit=100&plimit=10&fields='+v4+'&client_id=client_id&client_secret=client_secret')
             response = request.read()
             import json
             json = json.loads(response)
             if json['success']:
-                result_aeris = json['response']
-                #print(result_aeris)
+                print(json)
+                result_aeris = json
             else:
                 print("An error occurred: %s" % (json['error']['description']))
                 request.close()
-                    
-            #print(os.getcwd())
+
+            #Output directory is given as the path
             os.chdir(str(outputdir) + '/' + str(request_lkup['_id']))
             path2 = os.getcwd()
-            #print('Getwd',path2)
+
+            #Into the text file we are appending the result of aeris
             with open("myfile.txt", "w") as file_2: 
                 file_2.write("Aeries weather\n")
             file_2.close()
@@ -208,10 +218,8 @@ def check_new(sc):
             x = os.listdir(path2)
             file_name = x[0]
             file_path = os.path.join(path2,file_name)
-            #print(file_path)
             
-            #Send Grid Mail
-            #print(tempF,solar_radiation,Wind_speed,Wind_direction)
+            #SendGrid Mail is sent with the result and text file attachment
             message = Mail(from_email='devteam-noreply@hashdev.in',to_emails=email,subject='Microclim.org',html_content= 'Your Request with Microclim.org is successful.<br> The result is attached below.')
             with open(file_path, 'rb') as f:
                     data = f.read()
@@ -219,7 +227,7 @@ def check_new(sc):
             encoded_file = base64.b64encode(data).decode()
             attachment = Attachment()
             attachment.file_content = FileContent(encoded_file)
-            attachment.file_name = FileName('Microclim.json')
+            attachment.file_name = FileName('Microclim.')
             attachment.disposition = Disposition('attachment')
             message.attachment = attachment
             try:
@@ -230,6 +238,7 @@ def check_new(sc):
                 print(response.headers)   
             except Exception as e:
                 print(e)
+                
             #Updating the status of the request to mailed
             requests.update_one({
                 '_id': request_lkup['_id']
@@ -237,10 +246,12 @@ def check_new(sc):
                 '$set': {
                 'status': "EMAILED"
                 }
-                }, upsert = False)
+                }, upsert = False) 
 
         #If the source is ERA5 
         if request_lkup['sourcetype'] == 'ERA5':
+            
+            #Parameters needed for ERA5 are extracted 
             start_day = request_lkup['enddate'][6:8]
             end_day = request_lkup['startdate'][6:8]
             start_month = request_lkup['startdate'][4:6]
@@ -254,15 +265,10 @@ def check_new(sc):
             interval = request_lkup['interval']
             output = request_lkup['outputformat']
             email = request_lkup['email']
-
             if output == 'netcdf' or output == 'csv':
                 output_format = 'netcdf'
             if output == 'GRIB':
                 output_format = output
-
-            #print("fomat",output_format)
-            #print("output",output)
-            
             if interval == 'Daily':
                 time = ['00:00']
             elif interval == '6 Hourly':
@@ -271,26 +277,25 @@ def check_new(sc):
                 time = ['00:00','12:00']
             elif interval == 'Hourly':
                 time = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']
-    
             variables = request_lkup['variable']
-            #print(variables)
 
-
+            #Function call for function_cds 
             result = function_cds(start_year,end_year,start_month,end_month,start_day,end_day,North,South,East,West,variables,output_format,time)
-            print("Result :",result)
+            #print("Result :",result)
+
+            #Changing the path to the output directory
             os.chdir(str(outputdir) + '/' + str(request_lkup['_id']))
             path = os.getcwd()
             print('Getwd',path)
-      
+
+            #The result is written onto the textfile and the result is downloaded in the output directory
             with open("myfile.txt", "w") as file1: 
                 file1.write(result.download())
             file1.close()
             x = os.listdir(path)
             file_name = x[0]
             file_path = os.path.join(path,file_name)
-            #print('path',path)
-            #print("file path",file_path)
-            #print('file_name',file_name)
+    
 
             if output == 'netcdf' or output == 'GRIB':
                 if output == 'netcdf':
@@ -299,8 +304,8 @@ def check_new(sc):
                     ext = '.grib'
 
                 send_file_name = 'Microclim'+ext
-                #print(send_file_name)
-                #Sendgrid mail
+               
+                #Sendgrid mail for output formats such as netCDF and GRIB
                 message = Mail(from_email='devteam-noreply@hashdev.in',to_emails=email,subject='Microclim.org',html_content= 'Your Request with Microclim.org is successful.<br><br> The result is attached below.<br><br>'+ str(result))
                 with open(file_path, 'rb') as f:
                     data = f.read()
@@ -320,6 +325,7 @@ def check_new(sc):
                     print(response.headers)   
                 except Exception as e:
                     print(e)
+                    
                 #Updating the status of the request to Emailed
                 requests.update_one({
                 '_id': request_lkup['_id']
@@ -329,23 +335,21 @@ def check_new(sc):
                 }
                 }, upsert = False)
 
+            #If the output format is csv
             if output == 'csv':
-                #print("csv here")
-                #file_conversion netCDF to 
+                
+                #input file [netCDF] directory and output file[csv] path is given here
                 netcdf_file_name = file_name
                 netcdf_file_in = path+'\\'+file_name
                 csv_file_out = path+'\\'+ netcdf_file_name[:-3] + '.csv'
 
+                #function for file_conversion netCDF to csv
                 ds = xr.open_dataset(netcdf_file_in)
                 df = ds.to_dataframe()
                 df.to_csv(csv_file_out)
-
                 file_name = netcdf_file_name[1:-3]+'.csv'
-                #print(file_name)
-                #print(netcdf_file_in)
-                print("csv_file",csv_file_out)
-                #print(local_storage_directory)
-                #Sendgrid Mail
+
+                #Sendgrid Mail for output format csv
                 message = Mail(from_email='devteam-noreply@hashdev.in',to_emails=email,subject='Microclim.org',html_content= 'Your Request with Microclim.org is successful.<br><br> The result is attached below.<br><br>'+ str(result))
                 with open(csv_file_out, 'rb') as f:
                     data = f.read()
